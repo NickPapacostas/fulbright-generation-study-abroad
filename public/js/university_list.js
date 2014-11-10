d7688ae71a768e25070db408f5bb15441b8b6d7a
Nicky P <np1249a@gmail.com>
2014-11-11 00:31:29 +0200

    list lookin fresh. sorting & filtering. big up NgTable

--- public/js/university_list.js
+++ public/js/university_list.js
@@ -1,15 +1,35 @@
 var universityList = angular.module('universityList', ['ngTable']);
 
-universityList.controller('UniversitiesController', function ($scope, $http, ngTableParams) {
-  $http.get('masters_in_greece.json')
-    .then(function (res) {
-      $scope.universities = res.data;
-      window.universities = res.data;
-    });
-  $scope.tableParams = new ngTableParams({
-      page: 1,            // show first page
-      count: 10           // count per page
+universityList.controller('UniversitiesController', function ($scope, $http, $filter, ngTableParams) {
+  $http.get('masters_in_greece.json').then(function (res) {
+    universityListInit(res.data)
   });
+
+  universityListInit = function(data){
+    $scope.data = data;
+    $scope.tableParams = new ngTableParams({
+      page: 1,            // show first page
+      count: 10,           // count per page
+      sorting: {
+        institution: 'asc'
+      }
+    }, {
+      total: $scope.data.length,
+
+      getData: function($defer, params) {
+        console.log(params.sorting());
+        var orderedData = params.sorting() ?
+            $filter('orderBy')(data, params.orderBy()) :
+            data;
+        orderedData = params.filter() ?
+                   $filter('filter')(orderedData, params.filter()) :
+                   orderedData;
+
+        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+      }
+    });    
+    window.universities = data;
+  };
   $scope.isLink = function(input) {
     return input instanceof Object
   }
